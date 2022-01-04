c5b95eca5c6590d400a529f544f55472b71e02f8
Patryk Rezler <p.rezler@piwik.pro>
2022-01-04 18:27:57 +0100

    Initialized Turborepo boilerplate

---
+++ apps/web/pages/index.tsx
@@ -0,0 +1,71 @@
+import { useState, useEffect } from 'react';
+import { Button } from 'ui';
+import Box from '@mui/material/Box';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Divider from '@mui/material/Divider';
+import InboxIcon from '@mui/icons-material/Inbox';
+import DraftsIcon from '@mui/icons-material/Drafts';
+import FirebaseContext from '../config/fire-config';
+
+export default function Web() {
+    const [todos,setTodos] = useState([]);
+  const [loading,   setLoading] =   useState<boolean>(true);
+
+  useEffect(() => {
+    FirebaseContext
+      .collection('setlists')
+      .onSnapshot(snap => {
+        const blogs = snap.docs.map(doc => ({
+          id:   doc.id,
+          ...doc.data()
+        }));
+        setTodos(blogs);
+      });
+  }, []);
+
+  console.log(todos);
+
+  return (
+    <Box sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+      <nav aria-label="main mailbox folders">
+        <List>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <InboxIcon />
+              </ListItemIcon>
+              <ListItemText primary="Inbox" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <DraftsIcon />
+              </ListItemIcon>
+              <ListItemText primary="Drafts" />
+            </ListItemButton>
+          </ListItem>
+        </List>
+      </nav>
+      <Divider />
+      <nav aria-label="secondary mailbox folders">
+        <List>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemText primary="Trash" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#simple-list">
+              <ListItemText primary="Spam" />
+            </ListItemButton>
+          </ListItem>
+        </List>
+      </nav>
+    </Box>
+  );
+}
